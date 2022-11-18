4b8f9baf76f24c664c0a2af41519a70ced1180a1
Yevgeniy Vesselovskiy <106279381+vgenkon@users.noreply.github.com>
2022-11-19 00:02:13 +0600

    Страница админки + запись сообщений в MySQL

--- index.php
+++ index.php
@@ -1,71 +1,89 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>GoBot - Admin panel</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">
+    <!-- Google web font "Open Sans" -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/demo.css"/>
+    <link rel="stylesheet" href="css/templatemo-style.css">
+    <script type="text/javascript" src="js/modernizr.custom.86080.js"></script>
+</head>
+<body>
+<div id="particles-js"></div>
+<ul class="cb-slideshow">
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+<div class="container-fluid">
+    <div class="row cb-slideshow-text-container ">
+        <div class="tm-content col-xl-6 col-sm-8 col-xs-8 ml-auto section">
+            <header class="mb-5"><h1>Поделитесь чем-то важным...</h1></header>
+            <P class="mb-5">Введите текст сообщения, который будет отправлен всем пользователям, подписанным на
+                <a href="https://t.me/GoBot">@GoBot</a> в Telegram</P>
+            <form action="<?php
 
-require_once(__DIR__ . '/connect_db.php');
+if (isset ($_POST['text'])) {
+    // Переменная с формы
+    $text = $_POST['text'];
 
-const TOKEN = '';
+    // Параметры для подключения
+    $db_host = "localhost";
+            $db_user = "root"; // Логин БД
+            $db_password = "root"; // Пароль БД
+            $db_base = 'send_db'; // Имя БД
+            $db_table = "messages"; // Имя Таблицы БД
 
-const BASE_URL = 'https://api.telegram.org/bot' . TOKEN . '/';
+            try {
+            // Подключение к базе данных
+            $db = new PDO("mysql:host=$db_host;dbname=$db_base", $db_user, $db_password);
+            // Устанавливаем корректную кодировку
+            $db->exec("set names utf8");
+            // Собираем данные для запроса
+            $data = array('text' => $text);
+            // Подготавливаем SQL-запрос
+            $query = $db->prepare("INSERT INTO $db_table (text) values (:text)");
+            // Выполняем запрос с данными
+            $query->execute($data);
+            // Запишем в переменую, что запрос отработал
 
-$update = json_decode(file_get_contents('php://input'));
+            $result = true;
+            } catch (PDOException $e) {
+            // Если есть ошибка соединения или выполнения запроса, выводим её
+            print "Ошибка!: " . $e->getMessage() . "<br/>";
+            }
 
-file_put_contents(__DIR__ . '/logs.txt', print_r($update, 1), FILE_APPEND);
+            if ($result) {
+            echo "Сообщение успешно отправлено";
+            }
+            }
+            ?>" method="POST" class="subscribe-form" >
+                <div class="row form-section">
+                    <div class="col-md-7 col-sm-7 col-xs-7">
+                        <input name="text" type="text" class="form-control" id="text"
+                               placeholder="Сообщение..." required/>
+                    </div>
+                    <div class="col-md-5 col-sm-5 col-xs-5">
+                        <button type="send" class="tm-btn-subscribe">Отправить</button>
+                    </div>
 
-try
-{
-    $pdo = new PDO($dsn, $user, $pass, $opt);
-
-
-
-
-    $data = [
-        'username' => $username = $update->message->chat->username,
-        'telegram_id' => $update->message->chat->id,
-        'first_name' => $update->message->chat->first_name,
-        'last_name' => $update->message->chat->last_name
-    ];
-
-    $sql = "INSERT INTO users (username, telegram_id, first_name, last_name) VALUE
-        (:username, :telegram_id, :first_name, :last_name)";
-
-    $query = $pdo->prepare($sql);
-    $query->execute($data);
-}
-catch (PDOException $e)
-{
-    print('Подключение не удалось: ' . $e->getMessage());
-}
-
-
-$telegram_id = $update->message->chat->id;
-$username = $update->message->chat->username;
-$first_name = $update->message->chat->first_name;
-$last_name = $update->message->chat->last_name;
-
-
-
-
-
-$privet = ["Привет!", "привет!", "привет", "Привет"];
-
-if (in_array($update->message->text, $privet)) {
-    send_request('sendMessage', [
-        'chat_id' => $update->message->chat->id,
-        'text' => "Привет, <b>{$update->message->from->first_name}</b>!",
-        'parse_mode' => 'HTML',
-    ]);
-} else {
-    send_request('sendMessage', [
-        'chat_id' => $update->message->chat->id,
-        'text' => "Вы написали: <i>{$update->message->text}</i>",
-        'parse_mode' => 'HTML',
-    ]);
-}
-
-function send_request($method, $params = [])
-{
-    $url = BASE_URL . $method;
-    if (!empty($params)) {
-        $url = BASE_URL . $method . '?' . http_build_query($params);
-    }
-    return json_decode(file_get_contents($url));
-}
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="footer-link">
+        <p>Copyright © 2022 @GoBot - Финальный проект курса Backend-разработка-Kolesa Upgrade
+    </div>
+</div>
+</body>
+<script type="text/javascript" src="js/particles.js"></script>
+<script type="text/javascript" src="js/app.js"></script>
+</html>
